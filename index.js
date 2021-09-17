9d86e26726e14280ffb55a57a3ebd315ae772a12
Harsh Kumar <harshniet16@gmail.com>
2021-09-18 00:50:26 +0530

    form_validation

---
+++ index.js
@@ -0,0 +1,34 @@
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let flag = 1;
+
+function validateForm() {
+    if(username.value == '') {
+        document.getElementById('userError').innerHTML = 'username is empty';
+        flag = 0;
+    }
+    else if(username.value.length < 3) {
+        document.getElementById('userError').innerHTML = 'username require min 3 characters';
+        flag = 0;
+    }
+    else {
+        document.getElementById('userError').innerHTML = '';
+        flag = 1;
+    }
+
+    if(password.value == '') {
+        document.getElementById('passwordError').innerHTML ='password is empty';
+        flag = 0;
+    }
+    else {
+        document.getElementById('passwordError').innerHTML = '';
+        flag = 1;
+    }
+    
+    if(flag) {
+        return true;
+    }
+    else {
+        return false;
+    }
+} 
